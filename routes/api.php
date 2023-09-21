b5c090bd4da5d39a680e16cd2d999232b996bd27
unknown <ranggahendarmin07@gmail.com>
2023-09-21 21:40:55 +0700

    Pembuatan controller comment

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/posts', [PostController::class, 'store']);
     Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware('user-post');
     Route::delete('/delete/{id}', [PostController::class, 'destroy'])->middleware('user-post');
+
+    Route::post('/comment', [CommentController::class, 'store'])->middleware('user-post');
 });
 
 Route::get('/posts', [PostController::class, 'index']);
